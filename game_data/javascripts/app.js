6e50e0c64585e5fc69fd8678a2def5cc2843aa4f
Jalyna <jalyna.schroeder@gmail.com>
2015-01-25 11:02:27 +0100

    default effect

--- game_data/javascripts/app.js
+++ game_data/javascripts/app.js
@@ -112,12 +112,13 @@ socket.on('render_step', function(data) {
     console.log(data);
     if(data.text !== undefined) {
       $("#text").html("<span>" + data.text + "</span>");
-      if(data.text_effect || data.text_effect_out || data.text_type) {
-        if(!data.text_type) {
-          data.text_type = 'char';
-        }
-        $("#text > span").textillate({ in: data.text_effect, minDisplayTime: data.text_time, out: data.text_effect_out, type: data.text_type });
+      if(!data.text_type) {
+        data.text_type = 'char';
       }
+      if(!data.text_effect) {
+        data.text_effect = { effect: 'flipInY' };
+      }
+      $("#text > span").textillate({ in: data.text_effect, minDisplayTime: data.text_time, out: data.text_effect_out, type: data.text_type });
     }
     if(data.background !== undefined) {
       $("#background").css("background-image", "url(images/" + data.background + ")");
